659342676540ad617b2feafeb03b2733ea3ead58
Severin Rosner <einsevspielt@gmail.com>
2025-03-24 20:02:21 +0100

    feat: create new game

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import Game from '../views/Game.vue'
+import CreateGame from '@/views/CreateGame.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,6 +11,7 @@ const router = createRouter({
       name: 'home',
       component: HomeView,
     },
+    { path: '/games/new', component: CreateGame },
     { path: '/games/:id', component: Game },
   ],
 })
---
+++ frontend/src/views/CreateGame.vue
@@ -0,0 +1,34 @@
+<script setup>
+import Team from '@/components/Team.vue';
+import { useGameStore } from '@/stores/game';
+import { ref } from 'vue';
+
+const game = useGameStore();
+
+
+
+function log() {
+    console.log(game.teams);
+}
+</script>
+
+<template>
+    <div class="flex flex-col">
+        <div class="flex flex-col items-center mt-20">
+            <h1 class="font-bold text-4xl">Create new game</h1>
+        </div>
+        <form @submit.prevent="log">
+            <div class="flex flex-row justify-center mt-10">
+                <div class="w-1/3 flex flex-row justify-between gap-x-10">
+                    <Team :id="1" @updated-usernames="game.updateTeam" />
+                    <Team :id="2" @updated-usernames="game.updateTeam" />
+                </div>
+            </div>
+            <div class="mt-10 flex justify-center">
+                <button type="submit"
+                    class="px-3 py-2 bg-green-600 hover:bg-green-500 rounded-xl hover:cursor-pointer">Start
+                    game</button>
+            </div>
+        </form>
+    </div>
+</template>
