aa7a9e5aa403c20766725d88304fa45b2dfe97e6
Severin Rosner <einsevspielt@gmail.com>
2025-03-24 17:01:56 +0100

    feat: basic home page and game page setup

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Game from '../views/Game.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,8 +10,7 @@ const router = createRouter({
       name: 'home',
       component: HomeView,
     },
-    {
-    },
+    { path: '/games/:id', component: Game },
   ],
 })
 
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,7 +1,25 @@
 <script setup>
+import { ref } from 'vue'
+import { RouterLink } from 'vue-router'
 
+const games = ref([{ id: 1 }, { id: 2 }, { id: 3 }]);
 </script>
 
 <template>
-    Home
+    <div class="flex flex-col">
+        <div class="flex flex-col items-center mt-20">
+            <span class="text-2xl">Welcome to</span>
+            <h1 class="font-bold text-5xl">TableVision</h1>
+            <div class="mt-10">
+                <button class="px-3 py-2 bg-green-600 hover:bg-green-500 rounded-xl hover:cursor-pointer">Start new
+                    round</button>
+            </div>
+        </div>
+        <div class="flex flex-col items-center mt-10">
+            <h2>Last Rounds</h2>
+            <div class="flex flex-col gap-y-4">
+                <RouterLink v-for="game in games" :to="'/games/' + game.id">Round {{ game.id }}</RouterLink>
+            </div>
+        </div>
+    </div>
 </template>
